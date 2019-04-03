aac2cae6dbf7ad2fa620917c203bdb67c6f0c1ca
Abidujjaman Ovi <oviice@gmail.com>
2019-04-03 10:58:58 +0600

    RecyclerView Demo

--- app/src/main/java/com/example/recyclerview01/MainActivity.java
+++ app/src/main/java/com/example/recyclerview01/MainActivity.java
@@ -4,16 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.View;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    MyAdepter myAdepter;
+    MyAdapter myAdapter;
     RecyclerView recyclerView;
     String [] PersonName,dis;
-    int [] img={R.drawable.ripon,R.drawable.ripon,R.drawable.ripon,R.drawable.ripon,
-            R.drawable.ripon,R.drawable.ripon};
+    int [] img={R.mipmap.ic_launcher,R.mipmap.ic_launcher,R.mipmap.ic_launcher,
+            R.mipmap.ic_launcher,R.mipmap.ic_launcher,R.mipmap.ic_launcher};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,24 +20,8 @@ public class MainActivity extends AppCompatActivity {
         PersonName=getResources().getStringArray(R.array.pName);
         dis=getResources().getStringArray(R.array.disc);
 
-        myAdepter=new MyAdepter(this,PersonName,dis,img);
-        recyclerView.setAdapter(myAdepter);
+        myAdapter =new MyAdapter(this,PersonName,dis,img);
+        recyclerView.setAdapter(myAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        myAdepter.setOnItemClickListener(new MyAdepter.ClickListener() {
-            @Override
-            public void onItemClickListener(int position, View v) {
-
-                Toast.makeText(getApplicationContext(),"onItemClickListener is Clicked",Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onItemLongClickListener(int position, View v) {
-                Toast.makeText(getApplicationContext(),"onItemLongClickListener is Clicked",Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
-
     }
 }
--- app/src/main/java/com/example/recyclerview01/MyAdapter.java
+++ app/src/main/java/com/example/recyclerview01/MyAdapter.java
@@ -9,15 +9,12 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MyAdepter extends RecyclerView.Adapter<MyAdepter.MyViewHolder> {
-   private static ClickListener clickListener;
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     Context context;
-    RecyclerView recyclerView;
     String [] PersonName,dis;
     int [] img;
 
-    public MyAdepter(Context context, String[] personName, String[] dis, int[] img) {
-
+    public MyAdapter(Context context, String[] personName, String[] dis, int[] img) {
         this.context = context;
         PersonName = personName;
         this.dis = dis;
@@ -44,40 +41,14 @@ public class MyAdepter extends RecyclerView.Adapter<MyAdepter.MyViewHolder> {
     public int getItemCount() {
         return PersonName.length;
     }
-
-    class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
-
+    class MyViewHolder extends RecyclerView.ViewHolder{
         TextView textView1,textView2;
         ImageView imageView;
-
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             textView1=itemView.findViewById(R.id.textView1);
             textView2=itemView.findViewById(R.id.textView2);
             imageView=itemView.findViewById(R.id.imageView);
-
-            itemView.setOnClickListener(this);
-            itemView.setOnLongClickListener(this);
         }
-
-        @Override
-        public void onClick(View v) {
-
-            clickListener.onItemClickListener(getAdapterPosition(),v);
-        }
-
-        @Override
-        public boolean onLongClick(View v) {
-            clickListener.onItemLongClickListener(getAdapterPosition(),v);
-            return false;
-        }
-    }
-    public interface ClickListener{
-        void onItemClickListener(int position,View v);
-        void onItemLongClickListener(int position,View v);
-
-    }
-    public void setOnItemClickListener(ClickListener clickListener){
-       MyAdepter.clickListener=clickListener;
     }
 }
